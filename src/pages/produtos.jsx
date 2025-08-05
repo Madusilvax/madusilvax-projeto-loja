fb0a3148d928931f9462878878759438c7ca4a4f
Maria Eduarda <ma.eferreira20@gmail.com>
2025-08-04 22:20:23 -0300

    finalização de codigo dia 04.08.25

--- src/pages/produtos.jsx
+++ src/pages/produtos.jsx
@@ -16,33 +16,3 @@ export default function Produtos({ produtos, adicionarAoCarrinho }) {
     </main>
   )
 }
-
-```
-
-```
-//Carrinho.jsx
-
-export default function Carrinho({ carrinho }) {
-  const total = carrinho.reduce((soma, item) => soma + item.preco, 0)
-
-  return (
-    <main>
-      <h2>Carrinho de Compras</h2>
-
-      {carrinho.length === 0 ? (
-        <p>Seu carrinho está vazio.</p>
-      ) : (
-        <>
-          <ul>
-            {carrinho.map((item, index) => (
-              <li key={index}>
-                {item.nome} - R$ {item.preco}
-              </li>
-            ))}
-          </ul>
-          <p><strong>Total:</strong> R$ {total}</p>
-        </>
-      )}
-    </main>
-  )
-}
